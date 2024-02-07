58245fe6d5ae74968709a6328843087b482b99f5
kashish <arorakashish519@gmail.com>
2024-02-07 13:28:03 +0530

    Day 23

--- programs/8arrayhard/ele_more_than_n_by_3.cpp
+++ programs/8arrayhard/ele_more_than_n_by_3.cpp
@@ -24,7 +24,7 @@ set<int> brute(vector<int> a)
     }
     return s;
 }
-vector<int> majorityElement(vector<int> a) {
+vector<int> better(vector<int> a) {
 	vector<int> result;
     int n = a.size();
     map<int, int> mp;
@@ -41,10 +41,51 @@ vector<int> majorityElement(vector<int> a) {
 
     return result;
 }
+vector<int> optimal(vector<int> a) {
+    int n = a.size();
+    int count1=0,count2=0;
+    int ele=INT_MIN;
+    int ele2=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(count1==0 && ele2 !=a[i]){
+            count1=1;
+            ele=a[i];
+        }
+        else if(count2==0 && ele !=a[i]){
+            count2=1;
+            ele2=a[i];
+        }
+        else if(a[i]==ele)
+        count1++;
+        else if(a[i]==ele2)
+        count2++;
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    vector<int> v;
+    int cntele=0;int cntele2=0;
+    for(int i=0;i<n;i++){
+        if(ele==a[i]){
+        cntele++;
+        }
+        if(ele2==a[i]){
+        cntele2++;
+        }
+    }
+    if(cntele>n/3){
+        v.push_back(ele);
+    }
+    if(cntele2>n/3){
+        v.push_back(ele2);
+    }
+    return v;
+}
 int main()
 {
     vector<int> a = {1, 1, 1, 2, 2, 3, 3, 3};
-    set<int> res = brute(a);
+    vector<int> res =optimal(a);
     for (auto i : res)
     {
         cout << i << " ";
