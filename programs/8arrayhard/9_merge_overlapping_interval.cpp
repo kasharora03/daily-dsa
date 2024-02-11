44a9cb1ec46b731465acf7071f8d0db3225c117e
kashish <arorakashish519@gmail.com>
2024-02-11 18:55:34 +0530

    Day 27

---
+++ programs/8arrayhard/9_merge_overlapping_interval.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	int n = arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>> ans;
+	for(int i=0;i<n;i++){
+		int start=arr[i][0];
+		int end=arr[i][1];
+		if(!ans.empty() && end<=ans.back()[1]){
+			continue;
+		}
+		for(int j=i+1;j<n;j++){
+			if(arr[j][0]<=end){
+				end=max(end,arr[j][1]);
+			}
+			else{
+				break;
+			}
+		}
+		ans.push_back({start,end});
+	}
+	return ans;
+}
+int main(){
+    vector<vector<int>> v={{1,2},{4,5},{3,5},{2,8},{9,16},{10,11}};
+    vector<vector<int>> ans=mergeOverlappingIntervals(v);
+    for(auto x:ans) {
+        for(auto col:x){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+        }
+}
