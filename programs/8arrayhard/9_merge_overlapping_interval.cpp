34a8cdcbe75a733352d11e6ef6bf6a488dd896d9
kashish <arorakashish519@gmail.com>
2024-02-11 19:59:53 +0530

    Day 27

--- programs/8arrayhard/9_merge_overlapping_interval.cpp
+++ programs/8arrayhard/9_merge_overlapping_interval.cpp
@@ -23,9 +23,23 @@ vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
 	}
 	return ans;
 }
+vector<vector<int>> optimal(vector<vector<int>> &arr){
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+    vector<vector<int>>ans;
+    for(int i=0;i<n;i++){
+        if(ans.empty() || arr[i][0]> ans.back()[1]){
+            ans.push_back(arr[i]);
+        }
+        else{
+            ans.back()[1]=max(ans.back()[1],arr[i][1]);
+        }
+    }
+    return ans;
+}
 int main(){
     vector<vector<int>> v={{1,2},{4,5},{3,5},{2,8},{9,16},{10,11}};
-    vector<vector<int>> ans=mergeOverlappingIntervals(v);
+    vector<vector<int>> ans=optimal(v);
     for(auto x:ans) {
         for(auto col:x){
             cout<<col<<" ";
