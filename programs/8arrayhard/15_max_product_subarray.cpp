f2f79c9ac7e527131eae963768f08d183f8feb13
kashish <arorakashish519@gmail.com>
2024-02-15 19:34:20 +0530

    Day 31

--- programs/8arrayhard/15_max_product_subarray.cpp
+++ programs/8arrayhard/15_max_product_subarray.cpp
@@ -1,22 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-int brute(vector <int> a){
-    int maxi=INT_MIN;
-    int n=a.size();
-    int product;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            product=1;
-            for(int k=i;k<j;k++){
-                product=product*a[k];
+int brute(vector<int> a)
+{
+    int maxi = INT_MIN;
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int product = 1;
+            for (int k = i; k <= j; k++)
+            {
+                product = product * a[k];
+                maxi = max(product, maxi);
             }
-            maxi=max(product,maxi);
+            maxi = max(product, maxi);
         }
     }
     return maxi;
 }
-int main(){
-    vector <int> a={2,4,-6,4};
-    int res=brute(a);
-    cout<<res;
+int better(vector<int> a)
+{
+    int maxi = INT_MIN;
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+    {
+        int product = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            
+            product = product * a[j];
+            maxi = max(product, maxi);
+        }
+        maxi = max(product, maxi);
+    }
+    return maxi;
+}
+int optimal(vector <int > &a){
+    int pre=1,suff=1;
+    int maxproduct=INT_MIN;
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        if(pre==0){
+            pre=1;
+        }
+        if(suff==0){
+            suff=1;
+        }
+        pre=pre*a[i];
+        suff=suff*a[n-i-1];
+        maxproduct=max(maxproduct,max(pre,suff));
+    }
+    return  maxproduct;
+}
+int main()
+{
+    vector<int> a = {2};
+    int res = brute(a);
+    cout << res;
 }
