65d746874d3650192e390b9fcd5a60664ced87d3
kashish <arorakashish519@gmail.com>
2024-02-18 21:02:49 +0530

    Day 34

--- programs/9BinarySearch/06_first_and_last_occurence.cpp
+++ programs/9BinarySearch/06_first_and_last_occurence.cpp
@@ -1,7 +1,20 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
+pair<int, int> brute(vector<int>& arr, int n, int k)
+{
+    // tc=o(n)
+    int first=-1,last=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){
+            if(first==-1){
+                first=i;
+            }
+            last=i;
+        }
+    }
+    return {first,last};
+}
+// method 2 using lower and upper bound to reduce to 2 log2 n
 int upperBound(vector<int> &arr, int n, int x) {
     int low = 0, high = n - 1;
     int ans = n;
@@ -37,21 +50,72 @@ int lowerBound(vector<int> &arr, int n, int x) {
     }
     return ans;
 }
-
-
-pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+pair<int, int> better(vector<int>& arr, int n, int k) {
     int lb = lowerBound(arr, n, k);
     if (lb == n || arr[lb] != k) return { -1, -1};
     int ub = upperBound(arr, n, k);
     return {lb, ub - 1};
 }
+// methos 2 ends
+
+
+// method 3 to have a binary search for both first and last index
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) {
+            first = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k) {
+            last = mid;
+            //look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> optimal(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
 
 
 int main()
 {
-    vector<int> arr =  {2, 4, 6, 8,5, 8, 8, 11, 13};
-    int n = 8, k = 8;
-    pair<int, int> ans = firstAndLastPosition(arr, n, k);
+    vector<int> arr =  {2, 4, 6, 8, 8, 8, 11, 13};
+    int n = arr.size(), k = 7;
+    pair<int, int> ans = optimal(arr, n, 8);
     cout << "The first and last positions are: "
          << ans.first << " " << ans.second << "\n";
     return 0;
