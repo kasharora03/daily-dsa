9551d981e9e1e88281eac6fbaeb6dfadc109d7f7
kashish <arorakashish519@gmail.com>
2024-02-23 22:40:54 +0530

    Day 39

--- programs/10BinarySearchonAnswers/06_find_nth_missing_number_with_non_consecutive.cpp
+++ programs/10BinarySearchonAnswers/06_find_nth_missing_number_with_non_consecutive.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int missingK(vector < int > vec, int n, int k) {
+int brute(vector < int > vec, int n, int k) {
     for(int i=0;i<n;i++){
         if(vec[i]<=k){
             k++;
@@ -28,8 +28,8 @@ int missingK(vector < int > vec, int n, int k) {
 
 int main()
 {
-    vector<int> vec = {4, 7, 9, 10};
-    int n = 4, k = 4;
+    vector<int> vec = {2,3,8};
+    int n = 3, k = 1;
     int ans = missingK(vec, n, k);
     cout << "The missing number is: " << ans << "\n";
     return 0;
